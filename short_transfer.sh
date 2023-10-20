82f36f6cd8376a8ded2232f65d1e4073617c3be2
Christina Koch <christinakconnect@gmail.com>
2023-10-20 10:32:57 -0500

    new readme and files

--- short_transfer.sh
+++ short_transfer.sh
@@ -4,11 +4,11 @@ printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
-printf "The command line argument is: " $1
+printf "The command line argument is: " $@
+printf "Job number is" $2
 printf "Contents of $1 is "; cat $1
-cat $1 > output.txt
+cat $1 > output$2.txt
 echo
-echo "Working hard..."
-ls -l $PWD
+echo "Working hard...als -l $PWD
 sleep 20
 echo "Science complete!"
